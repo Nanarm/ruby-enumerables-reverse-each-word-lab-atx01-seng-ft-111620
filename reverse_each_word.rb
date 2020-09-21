f0f00c58d7691ec8277d9375e5ffdf8d38abe912
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-21 03:05:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
   new_string = []
 
   string_b.each do |backwards|
-    string_b = string.split.reverse
-    new_string << backwards.reverse
+    backwards = string.split.reverse
+    new_string << backwards
     
   end
 end
